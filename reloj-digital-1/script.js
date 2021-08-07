2056c4297a9bc5671832de2fba9ed95e262a3915
Hernando Abella <hernandoabella@gmail.com>
2021-08-07 10:51:19 -0500

    e

--- reloj-digital-1/script.js
+++ reloj-digital-1/script.js
@@ -52,16 +52,3 @@ function displayNumber(el, number) {
 		}
 	});
 }
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
